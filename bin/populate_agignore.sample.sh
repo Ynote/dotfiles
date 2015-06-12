61e163c8e521960181994c8720a55d73219a2ef5
Fanny Cheung <f.cheung.hk@gmail.com>
2015-06-12 15:41:07 +0200

    add script to populate .agignore for Unite

---
+++ bin/populate_agignore.sample.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Thanks to inside for this script:
+# http://insidesblog.blogspot.fr/2013/07/unitevim-and-many-files-in-project.html
+
+APP_DIR=path_to_your_project
+AG_IGNORE_FILE=.agignore
+
+# A list of directories or files you want unite.vim to inspect.
+# You can put as many as you like, separated by a pipe.
+WHITE_LIST='directory_1|directory_2|directory_3'
+
+ls -1 $APP_DIR | grep --color=never -v -P "($WHITE_LIST)" > ~/$AG_IGNORE_FILE
